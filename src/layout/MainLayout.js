fdd0bf752e41f37e477c1545f4d9c2582a850815
hevedrios <exxed.ridel@gmail.com>
2023-04-10 19:13:19 -0500

    ADD layout configuration and page components integration

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,16 @@
+import Header from '@/components/Header';
+import Nav from '@/common/Nav';
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <div className="min-h-full">
+        <main>
+          <Header />
+          <Nav />
+          <div className="max-w-7x1 mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </>
+  );
+}
